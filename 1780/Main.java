7ad7a39b981ae5f622a6d5305812c130e2eff8ab
wonsik <cws0173@gmail.com>
2019-09-03 08:52:05 +0900

    1780 solved recurssion problem

---
+++ 1780/Main.java
@@ -0,0 +1,93 @@
+/*
+    baekjoon online judge
+    problem number 1780
+    https://www.acmicpc.net/problem/1780
+*/
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+import java.io.OutputStreamWriter;
+import java.io.BufferedWriter;
+import java.util.StringTokenizer;
+import java.io.IOException;
+
+public class Main{
+    static int[][] arr;
+    static int[] numOfColor;
+    public static void main(String[] args)throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        numOfColor = new int[]{0,0,0};  // num of -1,0,1
+        String str = br.readLine();
+        int N = Integer.parseInt(str);
+        arr = new int[N][N];
+
+        for(int i=0;i<N;i++){
+            str = br.readLine();
+            StringTokenizer st = new StringTokenizer(str);
+            for(int j=0;j<N;j++){
+                arr[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+        /* test code for getColor method
+        bw.write(Integer.toString(getColor(1,1,1))+"\n");  //1
+        bw.write(Integer.toString(getColor(0,0,2))+"\n");  //1
+        bw.write(Integer.toString(getColor(2,2,2))+"\n");  //0
+        bw.write(Integer.toString(getColor(4,4,4))+"\n");  //1
+        bw.write(Integer.toString(getColor(0,4,4))+"\n");  //-1
+        */
+        
+        getColoredPaper();
+
+        bw.write(Integer.toString(numOfColor[0])+"\n");
+        bw.write(Integer.toString(numOfColor[1])+"\n");
+        bw.write(Integer.toString(numOfColor[2])+"\n");
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+
+    public static int getColor(int rowStart, int colStart, int length){
+        int color = arr[rowStart][colStart];
+        boolean flag = true;
+        // System.out.printf("row: %d-%d, column: %d-%d\n", rowStart, rowEnd, colStart, colEnd);
+        for(int i=rowStart; flag && i<rowStart+length; i++)
+            for(int j=colStart; flag && j<colStart+length; j++){
+                // System.out.printf("i: %d, j: %d\n",i,j);
+                if(arr[i][j] != color){
+                    flag = false;
+                    // System.out.println(arr[i][j]+" "+color);
+                }
+            }
+        if(!flag)
+            return -1;
+        return color+1;    // return value is -1,0,1
+    }
+
+    public static void getColoredPaper(){
+        __getColoredPaper(0,0,arr.length);
+    }
+
+    public static void __getColoredPaper(int rowStart, int colStart, int length){
+        // System.out.printf("row: %d-%d, column: %d-%d\n", rowStart, rowStart+length-1, colStart, colStart+length-1);
+        int color = getColor(rowStart, colStart, length);
+        
+        if(color == 0)
+            numOfColor[0]++;
+        else if(color == 1)
+            numOfColor[1]++;
+        else if(color == 2)
+            numOfColor[2]++;
+        else{
+            __getColoredPaper(rowStart, colStart, length/3);
+            __getColoredPaper(rowStart, colStart+length/3, length/3);
+            __getColoredPaper(rowStart, colStart+2*length/3, length/3);
+            __getColoredPaper(rowStart+length/3, colStart, length/3);
+            __getColoredPaper(rowStart+length/3, colStart+length/3, length/3);
+            __getColoredPaper(rowStart+length/3, colStart+2*length/3, length/3);
+            __getColoredPaper(rowStart+2*length/3, colStart, length/3);
+            __getColoredPaper(rowStart+2*length/3, colStart+length/3, length/3);
+            __getColoredPaper(rowStart+2*length/3, colStart+2*length/3, length/3);
+        }
+    }
+}
