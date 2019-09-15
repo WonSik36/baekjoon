54884e0379e522376a991567b5b6165e2fc51e1d
wonsik <cws0173@gmail.com>
2019-09-15 16:01:08 +0900

    update format.java
    add method print. it prints only DEBUG is true.

--- format.java
+++ format.java
@@ -11,8 +11,10 @@ import java.util.StringTokenizer;
 import java.io.IOException;
 
 public class Main{
-    static final boolean DEBUG = false;
+    static boolean DEBUG = false;
     public static void main(String[] args)throws IOException{
+        if(args.length!=0 && args[0].equals("-d"))
+            DEBUG = true;
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         
@@ -21,5 +23,24 @@ public class Main{
         bw.flush();
         bw.close();
         br.close();
+    }
+
+    public static void print(String str){
+        if(DEBUG)
+            System.out.print(str);
+    }
+
+    public static void print(String str, int... ints){
+        if(DEBUG){
+            String[] spl = str.split("%d");
+            String output = spl[0];
+            int idx = 1;
+            for(int number:ints){
+                output += number;
+                if(idx < spl.length)
+                    output += spl[idx++];
+            }
+            System.out.print(output);
+        }
     }
 }
