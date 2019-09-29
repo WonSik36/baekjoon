32ec20bd6ee81e90eab8684066a06d08e364834c
wonsik <cws0173@gmail.com>
2019-09-30 08:02:20 +0900

    12761 solved

--- 12761/Main.java
+++ 12761/Main.java
@@ -3,7 +3,8 @@
     problem number 12761
     https://www.acmicpc.net/problem/12761
     https://jaimemin.tistory.com/947
-    using BFS
+    using BFS and LinkedList
+    when using ArrayList timeout happened because of remove(0) method
 */
 import java.io.InputStreamReader;
 import java.io.BufferedReader;
@@ -11,11 +12,10 @@ import java.io.OutputStreamWriter;
 import java.io.BufferedWriter;
 import java.util.StringTokenizer;
 import java.io.IOException;
-import java.util.Arrays;
+import java.util.LinkedList;
 
 public class Main{
     static boolean DEBUG = false;
-    static final int MAX = 100000;
     public static void main(String[] args)throws IOException{
         if(args.length!=0 && args[0].equals("-d"))
             DEBUG = true;
@@ -29,7 +29,7 @@ public class Main{
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
 
-        int min = backTrace(A,B,N,M);
+        int min = BFS(A,B,N,M);
 
         bw.write(Integer.toString(min));
         bw.flush();
@@ -37,65 +37,44 @@ public class Main{
         br.close();
     }
 
-    public static int dp(int A, int B, int N, int M){
-        int[] rock = new int[MAX+1];
-        Arrays.fill(rock, MAX);
-        rock[N] = 0;
-        
-        if(N < M){
-            for(int i=N;i<=MAX;i++){
-                if(i+1 <= MAX)
-                    rock[i+1] = MIN(rock[i+1],rock[i]+1);
-                if(i+A <= MAX)
-                    rock[i+A] = MIN(rock[i+A],rock[i]+1);
-                if(i+B <= MAX)
-                    rock[i+B] = MIN(rock[i+B],rock[i]+1);
-                if(i*A <= MAX)
-                    rock[i+A] = MIN(rock[i*A],rock[i]+1);
-                if(i*B <= MAX)
-                    rock[i+B] = MIN(rock[i+1],rock[i]+1);
-            }
-        }else if(N > M){
+    public static int BFS(int A, int B, int N, int M){
+        LinkedList<Node> list = new LinkedList<Node>();
+        boolean[] visited = new boolean[100001];
+        Node tmp = null;
 
-        }else{
-            return 0;
+        list.addLast(new Node(N,0));
+        while(true){
+            tmp = list.remove();
+            
+            if(tmp.level == M)
+                break;
+            else if(tmp.level < 0 || tmp.level > 100000)
+                continue;
+            else if(visited[tmp.level])
+                continue;
+            else{
+                visited[tmp.level] = true;
+                list.addLast(new Node(tmp.level+1, tmp.cnt+1));
+                list.addLast(new Node(tmp.level-1, tmp.cnt+1));
+                list.addLast(new Node(tmp.level+A, tmp.cnt+1));
+                list.addLast(new Node(tmp.level-A, tmp.cnt+1));
+                list.addLast(new Node(tmp.level+B, tmp.cnt+1));
+                list.addLast(new Node(tmp.level-B, tmp.cnt+1));
+                list.addLast(new Node(tmp.level*A, tmp.cnt+1));
+                list.addLast(new Node(tmp.level*B, tmp.cnt+1));
+            }
         }
+
+        return tmp.cnt;
     }
 
-    public static int MIN(int a, int b){
-        return a<b?a:b;
-    }
+    public static class Node{
+        public int level;
+        public int cnt;
 
-    public static void print(int num){
-        if(DEBUG)
-            System.out.println(num);
-    }
-
-    public static void print(String str){
-        if(DEBUG)
-            System.out.print(str);
-    }
-
-    public static void print(String str, int... ints){
-        if(DEBUG){
-            String[] spl = str.split("%d");
-            String output = spl[0];
-            int idx = 1;
-            for(int number:ints){
-                output += number;
-                if(idx < spl.length)
-                    output += spl[idx++];
-            }
-            System.out.print(output);
-        }
-    }
-
-    public static void printArray(int[] arr){
-        if(DEBUG){
-            for(int i=0;i<arr.length;i++){
-                System.out.print(arr[i]+" ");
-            }
-            System.out.println();
+        Node(int level, int cnt){
+            this.level = level;
+            this.cnt = cnt;
         }
     }
 }
