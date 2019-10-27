65fc95953e0aae4be5c92c3bdf60d92722772286
wonsik <cws0173@gmail.com>
2019-10-27 11:27:01 +0900

    problem number 2098     https://www.acmicpc.net/problem/2098     https://hsp1116.tistory.com/40
        high level reference
    
        Travelling Salesperson Problem
        start from arbitrary one node(1st node in src code),
        travel whole nodes and finally back to start node

--- 2098/Main.java
+++ 2098/Main.java
@@ -4,7 +4,11 @@
     https://www.acmicpc.net/problem/2098
     https://hsp1116.tistory.com/40
 
+    high level reference
+
     Travelling Salesperson Problem
+    start from arbitrary one node(1st node in src code), 
+    travel whole nodes and finally back to start node
 */
 
 // import java.io.FileReader;
@@ -18,91 +22,121 @@ import java.io.IOException;
 import java.util.Arrays;
 
 public class Main{
-    static boolean DEBUG = false;
-    static final int INF = 10000000;
-    static final int MAX_MASK = 65535; // 2^16-1
+    static int N;
+    static int MAX_MASK; // 2^N-1
     static final int MIN_MASK = 0;
+    static final int FST_NODE = 1;
+    static final int INF = Integer.MAX_VALUE;
     public static void main(String[] args)throws IOException{
-        if(args.length!=0 && args[0].equals("-d"))
-            DEBUG = true;
         // BufferedReader br = new BufferedReader(new FileReader("./1.in"));
         // BufferedWriter bw = new BufferedWriter(new FileWriter("./1.out"));
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         
         // get N and make dist, and dp
-        int N = Integer.parseInt(br.readLine());
+        N = Integer.parseInt(br.readLine());
+        MAX_MASK = (1<<N)-1;
         int[][] dist = new int[N+1][N+1];
         int[][] dp = new int[N+1][(int)Math.pow(2, N)];
-        for(int i=1;i<=N;i++){
-            Arrays.fill(dist[i],INF);
-            dist[i][i] = 0;
-        }
 
         // get edges
         for(int i=1;i<=N;i++){
             StringTokenizer st = new StringTokenizer(br.readLine());
             for(int j=1;j<=N;j++){
-                int w = Integer.parseInt(st.nextToken());
-                if(w != 0)
-                    dist[i][j] = w;
+                dist[i][j] = Integer.parseInt(st.nextToken());
             }
         }
 
+        int min = TSP(FST_NODE,add(0,FST_NODE),dp,dist);
 
-
-
+        bw.write(Integer.toString(min)+"\n");
         bw.flush();
         bw.close();
         br.close();
     }
 
-    
+    // cur: current position, visited: visited nodes
+    public static int TSP(int cur, int visited, int[][] dp, int[][] dist){
+        // System.out.printf("current: %d visited: %s\n",cur,stringBit(visited));
+        if(visited == MAX_MASK){
+            if(dist[cur][FST_NODE]!=0)
+                return dist[cur][FST_NODE];
+            else
+                return INF;
+        }
+        
+        if(dp[cur][visited]!=0)
+            return dp[cur][visited];
+        int ret = INF;
+        for(int i=1;i<=N;i++){
+            if(check(visited,i))
+                continue;
+            if(dist[cur][i] == 0)
+                continue;
+            // System.out.println("Check next node "+i);
+            ret = Min(ret, saturateSum(TSP(i,add(visited,i),dp,dist),dist[cur][i]));
+        }
+        // System.out.printf("current: %d visited: %s return: %d\n",cur,stringBit(visited),ret);
+        dp[cur][visited] = ret;
+        return ret;
+    }   
 
+    public static int saturateSum(int a, int b){
+        long res = (long)a + b;
+        if(res > INF)
+            return INF;
+        else
+            return (int)res;
+    }
+
+    public static int Min(int a, int b){
+        return a<b?a:b;
+    }
+    
     public static int add(int bit, int idx){
-        int mask = 1<<idx;
+        int mask = 1<<(idx-1);
         return bit|mask;
     }
 
-    public static int remove(int bit, int idx){
-        int mask = 1;
-        for(int i=1;i<idx;i++){
-            mask *= 2;
-        }
-        mask = mask^MAX_MASK;
-        return bit&mask;
-    }
-
     public static boolean check(int bit, int idx){
-        int mask = 1;
-        for(int i=1;i<idx;i++){
-            mask *= 2;
-        }
+        int mask = 1<<(idx-1);
         int res = mask&bit;
         return res>0?true:false;
     }
 
-    public static int toggle(int bit, int idx){
-        int res;
-        if(check(bit,idx))
-            res = remove(bit,idx);
-        else
-            res = add(bit,idx);
-        return res;
-    }
+    public static String stringBit(int bit){
+        boolean[] res = new boolean[N];
+        String ret = "";
 
-    public static void printBit(int bit){
-        boolean[] res = new boolean[20];
-        
         // calculate result
-        for(int i=0;i<20;i++){
+        for(int i=0;i<N;i++){
             if(bit%2 == 1)
-                res[20-i-1] = true;
+                res[N-i-1] = true;
             bit /= 2;
         }
 
         // print it
-        for(int i=0;i<20;i++){
+        for(int i=0;i<N;i++){
+            if(res[i])
+                ret+="1";
+            else
+                ret+="0";
+        }
+        return ret;
+    }
+
+    public static void printBit(int bit){
+        boolean[] res = new boolean[N];
+        
+        // calculate result
+        for(int i=0;i<N;i++){
+            if(bit%2 == 1)
+                res[N-i-1] = true;
+            bit /= 2;
+        }
+
+        // print it
+        for(int i=0;i<N;i++){
             if(res[i])
                 System.out.print("1");
             else
