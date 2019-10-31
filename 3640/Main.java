005d70a3ce3d3cc5a2648a615bea612395a49771
wonsik <cws0173@gmail.com>
2019-10-31 19:18:05 +0900

    solve this problem later, network flow theory

---
+++ 3640/Main.java
@@ -0,0 +1,71 @@
+/*
+    baekjoon online judge
+    problem number 3640
+    https://www.acmicpc.net/problem/3640
+
+    MCMF(Minimum Cost Maximum Flow) Problem
+    https://www.crocus.co.kr/1090
+
+    SPFA(Shortest Path Faster Algorithm) Problem
+    https://www.crocus.co.kr/1089
+*/
+
+// import java.io.FileReader;
+// import java.io.FileWriter;
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+import java.io.OutputStreamWriter;
+import java.io.BufferedWriter;
+import java.util.StringTokenizer;
+import java.io.IOException;
+
+public class Main{
+    static boolean DEBUG = false;
+    public static void main(String[] args)throws IOException{
+        if(args.length!=0 && args[0].equals("-d"))
+            DEBUG = true;
+        // BufferedReader br = new BufferedReader(new FileReader("./1.in"));
+        // BufferedWriter bw = new BufferedWriter(new FileWriter("./1.out"));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        String str = br.readLine();
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+
+    public static void print(int num){
+        if(DEBUG)
+            System.out.println(num);
+    }
+
+    public static void print(String str){
+        if(DEBUG)
+            System.out.print(str);
+    }
+
+    public static void print(String str, int... ints){
+        if(DEBUG){
+            String[] spl = str.split("%d");
+            String output = spl[0];
+            int idx = 1;
+            for(int number:ints){
+                output += number;
+                if(idx < spl.length)
+                    output += spl[idx++];
+            }
+            System.out.print(output);
+        }
+    }
+
+    public static void printArray(int[] arr){
+        if(DEBUG){
+            for(int i=0;i<arr.length;i++){
+                System.out.print(arr[i]+" ");
+            }
+            System.out.println();
+        }
+    }
+}
