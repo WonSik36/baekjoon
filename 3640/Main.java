65efac4bd84a08ac3f64a230d892a2e7cb659892
wonsik <cws0173@gmail.com>
2019-11-01 15:45:47 +0900

    not solved

--- 3640/Main.java
+++ 3640/Main.java
@@ -3,8 +3,10 @@
     problem number 3640
     https://www.acmicpc.net/problem/3640
 
+    solve by MCMF but capacity is 1
+
     MCMF(Minimum Cost Maximum Flow) Problem
-    https://www.crocus.co.kr/1090
+    https://m.blog.naver.com/kks227/220810623254
 
     SPFA(Shortest Path Faster Algorithm) Problem
     https://www.crocus.co.kr/1089
@@ -12,60 +14,243 @@
 
 // import java.io.FileReader;
 // import java.io.FileWriter;
+
 import java.io.InputStreamReader;
 import java.io.BufferedReader;
 import java.io.OutputStreamWriter;
 import java.io.BufferedWriter;
 import java.util.StringTokenizer;
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.LinkedList;
 
 public class Main{
-    static boolean DEBUG = false;
+    static final int INF = 2000000000;
     public static void main(String[] args)throws IOException{
-        if(args.length!=0 && args[0].equals("-d"))
-            DEBUG = true;
         // BufferedReader br = new BufferedReader(new FileReader("./1.in"));
         // BufferedWriter bw = new BufferedWriter(new FileWriter("./1.out"));
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         
-        String str = br.readLine();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int V = Integer.parseInt(st.nextToken());
+        int E = Integer.parseInt(st.nextToken());
+        Graph g = new Graph(V);
 
+        for(int i=0;i<E;i++){
+            st = new StringTokenizer(br.readLine());
+            int start = Integer.parseInt(st.nextToken());
+            int end = Integer.parseInt(st.nextToken());
+            int weight = Integer.parseInt(st.nextToken());
+
+            g.addEdge(start, end, weight);
+        }
+
+        int sum = g.MCMF(1, V);
+
+        bw.write(Integer.toString(sum)+"\n");
         bw.flush();
         bw.close();
         br.close();
     }
 
-    public static void print(int num){
-        if(DEBUG)
-            System.out.println(num);
-    }
+    public static class Node{
+        private int nodeNum;
+        private int capacity;
+        private int weight;
+        private int flow;
+    
+        public Node(int nodeNum, int weight){
+            this(nodeNum,weight,1);
+        }
 
-    public static void print(String str){
-        if(DEBUG)
-            System.out.print(str);
-    }
+        public Node(int nodeNum, int weight, int capacity){
+            this.nodeNum = nodeNum;
+            this.weight = weight;
+            this.capacity = capacity;
+            this.flow = 0;
+        }
 
-    public static void print(String str, int... ints){
-        if(DEBUG){
-            String[] spl = str.split("%d");
-            String output = spl[0];
-            int idx = 1;
-            for(int number:ints){
-                output += number;
-                if(idx < spl.length)
-                    output += spl[idx++];
-            }
-            System.out.print(output);
+        public int getNodeNum(){
+            return nodeNum;
+        }
+    
+        public void setNodeNum(int nodeNum){
+            this.nodeNum = nodeNum;
+        }
+    
+        public int getWeight(){
+            return weight;
+        }
+    
+        public void setWeight(int weight){
+            this.weight = weight;
+        }
+
+        public int getCapacity(){
+            return capacity;
+        }
+    
+        public void setCapacity(int capacity){
+            this.capacity = capacity;
+        }
+    
+        public void addFlow(int flow){
+            this.flow += flow;
+            if(this.flow > this.capacity)
+                throw new AssertionError("Flow over Capacity");
+        }
+
+        public int getRemain(){
+            if(this.flow > this.capacity)
+                throw new AssertionError("Flow over Capacity");
+            return this.capacity - this.flow;
+        }
+
+        @Override
+        public boolean equals(Object obj){
+            Node that = (Node)obj;
+            if(this.nodeNum == that.getNodeNum())
+                return true;
+            return false;
         }
     }
 
-    public static void printArray(int[] arr){
-        if(DEBUG){
-            for(int i=0;i<arr.length;i++){
-                System.out.print(arr[i]+" ");
+    public static class Graph{
+        private ArrayList<ArrayList<Node>> adjList;
+        private int vertex;
+        private int edge;
+    
+        public Graph(int N){
+            this.vertex = N;
+            this.edge = 0;
+            adjList = new ArrayList<ArrayList<Node>>();
+            for(int i=0;i<N;i++){
+                adjList.add(new ArrayList<Node>());
             }
-            System.out.println();
+        }
+
+        public int MCMF(int start, int end){
+            int sum = 0;
+            boolean[] visited = new boolean[vertex+1];
+
+            //find augmented path
+            for(int k=0;k<2;k++){
+                int[] prev = new int[vertex+1];
+                int[] dist = new int[vertex+1];
+                boolean[] inQ = new boolean[vertex+1];
+                LinkedList<Node> queue = new LinkedList<Node>();
+                
+                Arrays.fill(dist,INF);
+                dist[start] = 0;
+                inQ[start] = true;
+                queue.addLast(new Node(start, 0));
+
+                // SPFA
+                while(!queue.isEmpty()){
+                    Node top = queue.removeFirst();
+                    inQ[top.getNodeNum()] = false; 
+                    Iterator<Node> it = getList(top.getNodeNum()).iterator();
+
+                    while(it.hasNext()){
+                        Node tmp = it.next();
+                        int tmpNum = tmp.getNodeNum();
+
+                        // 1. Node tmp is not visited
+                        // 2. edge from top to tmp should have remain flow
+                        // 3. edge from top to tmp + weight from start to top is shorter than weight from start to tmp
+                        if(!visited[tmpNum] && tmp.getRemain() > 0 && dist[tmpNum] > dist[top.getNodeNum()] + tmp.getWeight()){
+                            dist[tmpNum] = dist[top.getNodeNum()] + tmp.getWeight();
+                            prev[tmpNum] = top.getNodeNum();
+                            
+                            if(!inQ[tmpNum]){
+                                queue.addLast(tmp);
+                                inQ[tmpNum] = true;
+                            }
+                        }
+                    }
+                }
+
+                // there is no path from start to end
+                if(prev[end] == 0)
+                    break;
+                
+                // find minimum flow in the path
+                // check the node was visited without start and end
+                int minFlow = INF;
+                int totalWeight = 0;
+                for(int i=end;i!=start;i=prev[i]){
+                    visited[i] = true;
+                    int remain = getEdge(prev[i],i).getRemain();
+                    minFlow = minFlow<remain?minFlow:remain;
+                }
+                visited[start] = false;
+                visited[end] = false;
+
+                // add flow to every edge in the path
+                // get total weight
+                for(int i=end;i!=start;i=prev[i]){
+                    getEdge(prev[i],i).addFlow(minFlow);    // forward flow add
+                    getEdge(i,prev[i]).addFlow(minFlow*(-1)); // reverse flow add
+                    totalWeight += getEdge(prev[i],i).getWeight();
+                }
+
+                sum += minFlow*totalWeight;
+            }
+
+            return sum;
+        }
+    
+        public String toString(){
+            String ret = "";
+            for(int i=0;i<adjList.size();i++){
+                ArrayList<Node> list = adjList.get(i);
+                ret += ("Node["+(i+1)+"]: ");
+    
+                Iterator<Node> it = list.iterator();
+                while(it.hasNext()){
+                    Node node = it.next(); 
+                    int nodeNum = node.getNodeNum(); 
+                    int capacity = node.getCapacity();
+                    ret += String.format("%d[%d]", nodeNum, capacity);
+                }
+                ret += "\n";
+            }
+            return ret;
+        }
+    
+        public int getVertex(){
+            return vertex;
+        }
+    
+        public int getEdge(){
+            return edge;
+        }
+    
+        // directed graph and from u to v there can be only one edge
+        public void addEdge(int start, int end, int weight){
+            ArrayList<Node> list = adjList.get(start-1);
+            list.add(new Node(end, weight));
+            // list = adjList.get(end-1);
+            // list.add(new Node(start, (-1)*weight));
+            edge++;
+        }
+
+        public Node getEdge(int start, int end){
+            ArrayList<Node> list = getList(start);
+            for(int i=0;i<list.size();i++){
+                Node tmp = list.get(i);
+                if(tmp.getNodeNum() == end)
+                    return tmp;
+            }
+
+            throw new AssertionError("No node in adj List "+start);
+        }
+    
+        public ArrayList<Node> getList(int node){
+            return adjList.get(node-1);
         }
     }
 }
