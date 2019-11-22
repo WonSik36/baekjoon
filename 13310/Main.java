44fdd312529eb74e2f0e543a68cb75b1f35e59de
wonsik <cws0173@gmail.com>
2019-11-22 13:46:16 +0900

    not now

---
+++ 13310/Main.java
@@ -0,0 +1,34 @@
+/*
+    baekjoon online judge
+    problem number 13310
+    https://www.acmicpc.net/problem/13310
+    https://m.blog.naver.com/kks227/221432986308
+
+    Convex Hull:
+    Rotating Calipers:
+    Ternary Search:
+*/
+
+// import java.io.FileReader;
+// import java.io.FileWriter;
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+import java.io.OutputStreamWriter;
+import java.io.BufferedWriter;
+import java.util.StringTokenizer;
+import java.io.IOException;
+
+public class Main{
+    public static void main(String[] args)throws IOException{
+        // BufferedReader br = new BufferedReader(new FileReader("./1.in"));
+        // BufferedWriter bw = new BufferedWriter(new FileWriter("./1.out"));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        String str = br.readLine();
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
