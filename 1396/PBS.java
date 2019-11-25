15f1be39950b38edee681048fce3b1f3aa98f1ab
wonsik <cws0173@gmail.com>
2019-11-25 12:02:11 +0900

    not now

---
+++ 1396/PBS.java
@@ -0,0 +1,38 @@
+/*
+    baekjoon online judge
+    problem number 1396
+    https://www.acmicpc.net/problem/1396
+    https://handongproblemsolvers.github.io/2019/11/23/Week_13_Contest_Problem_Solving/#%ED%81%AC%EB%A3%A8%EC%8A%A4%EC%B9%BC%EC%9D%98-%EA%B3%B5
+
+    deep level reference
+
+    Parallel Binary Search Algorithm
+    http://kks227.blog.me/221410398513
+
+    Kruskal Algorithm
+    https://m.blog.naver.com/ndb796/221230994142
+*/
+
+// import java.io.FileReader;
+// import java.io.FileWriter;
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+import java.io.OutputStreamWriter;
+import java.io.BufferedWriter;
+import java.util.StringTokenizer;
+import java.io.IOException;
+
+public class Main{
+    public static void main(String[] args)throws IOException{
+        // BufferedReader br = new BufferedReader(new FileReader("./1.in"));
+        // BufferedWriter bw = new BufferedWriter(new FileWriter("./1.out"));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        String str = br.readLine();
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
