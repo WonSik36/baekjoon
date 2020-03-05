c1c51f7af1e9bcab05a8195b328ff8ce7bb45b99
wonsik <cws0173@gmail.com>
2020-03-05 13:24:26 +0900

    update format

--- format.java
+++ format.java
@@ -4,8 +4,6 @@
     https://www.acmicpc.net/problem/
 */
 
-// import java.io.FileReader;
-// import java.io.FileWriter;
 import java.io.InputStreamReader;
 import java.io.BufferedReader;
 import java.io.OutputStreamWriter;
@@ -15,8 +13,6 @@ import java.io.IOException;
 
 public class Main{
     public static void main(String[] args)throws IOException{
-        // BufferedReader br = new BufferedReader(new FileReader("./1.in"));
-        // BufferedWriter bw = new BufferedWriter(new FileWriter("./1.out"));
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         
