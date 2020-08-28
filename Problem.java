24fa6d3e3ff6668020ce97054142907a52a6ab24
wonsik <cws0173@gmail.com>
2020-08-28 11:17:20 +0900

    solve!!!!!!!!!!!

--- Problem.java
+++ Problem.java
@@ -1,20 +1,23 @@
 class Solution {
     public static void main(String[] args){
-        String[] input = {"aaaaa", "aaaab", "aaabb", "aabbb", "abbbb","bbbbb","aaabc","aabcc"};
+        String[] input = {"go", "gone", "guild"};
         Solution sol = new Solution();
         System.out.println(sol.solution(input));
     }
 
     public int solution(String[] words) {
         Node root = new Node(0);
+        root.skip = false;
         for(int i=0;i<words.length;i++){
             root.add(words[i]);
         }
 
+        // root.print();
+
         int sum = 0;
         for(int i=0;i<words.length;i++){
             int num = root.find(words[i]);
-            System.out.println(num);
+            // System.out.println(num);
             sum += num;
         }
 
@@ -25,41 +28,56 @@ class Solution {
 class Node {
     Node[] children;
     int totalChild;
-    boolean last;
     int level;
-    
+    boolean skip;
+
     public Node(int level){
         this.children = new Node[26];
         this.totalChild = 0;
-        this.last = false;
         this.level = level;
-    }
-
-    public int find(String word){
-        if(word.length() == this.level)
-            return 0;
-
-        int key = word.charAt(this.level) - 'a';
-        // if(this.level!=0 && totalChild==1 && !this.last){
-        if(totalChild==1 && !this.last){
-            return children[key].find(word);
-        }else{
-            return children[key].find(word) + 1;
-        }
+        this.skip = true;
     }
     
-    public void add(String word){
-        if(word.length() == this.level){
-            this.last = true;
-            return;
+    public void add(String str){
+        Node cur = this;
+
+        for(int i=0;i<str.length();i++){
+            int key = str.charAt(i) - 'a';
+
+            if(cur.children[key] == null)
+                cur.children[key] = new Node(cur.level+1);
+            else
+                cur.children[key].skip = false;
+
+            cur.totalChild++;
+            cur = cur.children[key];
+        }
+    }
+
+    public int find(String str){
+        Node cur = this;
+
+        for(int i=0;i<str.length();i++){
+            if(cur.skip)
+                return i;
+            
+            int key = str.charAt(i) - 'a';
+
+            cur = cur.children[key];
         }
 
-        int key = word.charAt(this.level) - 'a';
-        if(this.children[key] == null){
-            this.children[key] = new Node(this.level+1);
-        }
-        this.totalChild++;
+        return str.length();
+    }
 
-        children[key].add(word);
+    public void print(){
+        for(int i=0;i<26;i++){
+            if(this.children[i] == null)
+                continue;
+            
+            for(int j=0;j<this.level;j++)
+                System.out.print("-");
+            System.out.println((char)(i+'a'));
+            this.children[i].print();
+        }
     }
 }
